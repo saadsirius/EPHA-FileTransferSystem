ed110778bc95364d6b873b5a48916b3802f610e1
saadlamkadam <saad.lamkadam@gmail.com>
2022-01-01 23:08:19 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EPHA_FileTransferSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
